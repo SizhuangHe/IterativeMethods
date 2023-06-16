14dbaa807329c7bf4966f4a037cb3f71c9f7ddd5
SizhuangHe <sizhuang@umich.edu>
2023-06-16 19:15:49 -0400

    grid search

--- GCNexperiments/grid_search_hypparam.py
+++ GCNexperiments/grid_search_hypparam.py
@@ -21,12 +21,15 @@ logger.addHandler(file_handler)
 
 
 DS_NAME = ['Cora'] #only Cora for now
-LR = np.arange(0.001, 0.01, 0.0005)
-SMOOTH_FAC = np.arange(0, 1 , 0.1)
-WD = np.arange(1e-4, 1e-3, 1e-4)
-HID_DIM = [8, 16, 32, 64]
+LR = np.arange(0.005, 0.01, 0.0005) 
+SMOOTH_FAC = np.arange(0.5, 1 , 0.1) 
+HID_DIM = [8, 16, 32] # 3
+WD = [1e-4, 3e-4, 5e-4]
 num_runs = 3
 
+# total_experiments =  len(DS_NAME) * LR.size * SMOOTH_FAC.size * len(HID_DIM) * len(WD) *  num_runs
+total_experiments =  len(DS_NAME) * len(LR) * len(SMOOTH_FAC) * len(HID_DIM) * len(WD) *  num_runs
+print("{} sets of parameters to search from!".format(total_experiments))
 
 for ds_name in DS_NAME:
     logger.info("Experiment begins, dataset: {}".format(ds_name))
@@ -37,51 +40,48 @@ for ds_name in DS_NAME:
     running_max_acc = 0
     max_lr = -1
     max_sm_fac = -1
+    max_weight_decay = -1
+    max_hid_dim = -1
     curr_exp = 1
-    
+    num_param_set = 1
+   
     for lr in LR:
         for smooth_fac in SMOOTH_FAC:
-            run_acc = 0
-            for run in range(num_runs):
-                model = iterativeGCN(input_dim=dataset.num_features,
-                                        output_dim=dataset.num_classes,
-                                        hidden_dim=16,
-                                        num_train_iter=2,
-                                        num_eval_iter=2,
-                                        smooth_fac=smooth_fac,
-                                        dropout=0.5)
-                loss_test, acc_test, training_time = run_experiment(model=model, 
-                                                                    data=data, 
-                                                                    lr=lr, 
-                                                                    weight_decay=2e-4,
-                                                                    model_name=str(lr) + "_" + str(smooth_fac),
-                                                                    run=run,
-                                                                    num_epochs=200,
-                                                                    )
-                run_acc += acc_test
-                print("Experiment ", curr_exp,
-                      "run ", run + 1,
-                      ", total time: {:.4}".format(training_time),
-                        "learning rate: {:.4},".format(lr), 
-                        "smoothing factor: {:.4},".format(smooth_fac), 
-                        "test accuracy: {:.4}".format(acc_test))
-                del model
+            for hid_dim in HID_DIM:
+                for weight_decay in WD:
+                    run_acc = 0
+                    for run in range(num_runs):
+                        model = iterativeGCN(input_dim=dataset.num_features,
+                                                output_dim=dataset.num_classes,
+                                                hidden_dim=16,
+                                                num_train_iter=2,
+                                                num_eval_iter=2,
+                                                smooth_fac=smooth_fac,
+                                                dropout=0.5)
+                        loss_test, acc_test, training_time = run_experiment(model=model, 
+                                                                            data=data, 
+                                                                            lr=lr, 
+                                                                            weight_decay=2e-4,
+                                                                            model_name=str(lr) + "_" + str(smooth_fac),
+                                                                            run=run,
+                                                                            num_epochs=200,
+                                                                            )
+                        run_acc += acc_test
+                        print("Experiment {:04d}/{:04d} run {}/{} finished, time elapsed: {:.4}".format(curr_exp, total_experiments, run+1, num_runs, training_time))
+                        curr_exp += 1 
+                        del model
+                
+                    mean_acc = run_acc / num_runs
+                    
+                    logger.info("Parameter set {:04d}: hidden_dim = {:02d}, lr={:.4f}, smooth_fac={:.4f}, weight_decay = {:.4f}, mean test accuracy: {:.4}".format(num_param_set, hid_dim, lr, smooth_fac, weight_decay, mean_acc))
+                    num_param_set += 1
             
-            mean_acc = run_acc / num_runs
-            print("Experiment ", curr_exp, " summary: mean test accuracy:{:.4}".format(mean_acc))
-            print()
-            logger.info("Parameter set 1: hidden_dim = {:.4}, lr={:.4f}, smooth_fac={:.4f}, weight_decay = {:.4f}")
-            curr_exp += 1    
+                    if mean_acc > running_max_acc:
+                        running_max_acc = mean_acc
+                        max_lr = lr
+                        max_sm_fac = smooth_fac
+                        max_hid_dim = hid_dim
+                        max_weight_decay = weight_decay
             
-            if mean_acc > running_max_acc:
-                running_max_acc = mean_acc
-                max_lr = lr
-                max_sm_fac = smooth_fac
-            
-            
-
-    print("Summary:")
-    print("Best test accuracy: {:.4} ".format(running_max_acc),
-           ". Hyperparameters: learning rate: {:.4} ".format(max_lr),
-             ", smoothing factor: {:.4}".format(max_sm_fac))
-    print()
+    logger.info("--> Experiment finished. Best accuracy: {:.4f}, parameters: lr={:.4f}, smooth_fac={:.4f}, hid_dim={}, weight decay={:.2e}".format(running_max_acc, max_lr, max_sm_fac, max_hid_dim, max_weight_decay))
+    print("--> Experiment finished. Best accuracy: {:.4f}, parameters: lr={:.4f}, smooth_fac={:.4f}, hid_dim={}, weight decay={:.2e}".format(running_max_acc, max_lr, max_sm_fac, max_hid_dim, max_weight_decay))
