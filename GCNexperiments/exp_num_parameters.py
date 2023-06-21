fbe9fac5faaef504bcd794352753125d5fcf2ae9
SizhuangHe <sizhuang@umich.edu>
2023-06-21 15:55:01 -0400

    cm

--- GCNexperiments/exp_num_parameters.py
+++ GCNexperiments/exp_num_parameters.py
@@ -14,7 +14,7 @@ import wandb
 wandb.login()
 
 
-def add_noise(data, percent=0.7):
+def add_noise(data, percent=0):
     #add random 1's to data
     if percent > 0 and percent <= 1:
         len = np.prod(list(data.x.shape))
@@ -26,32 +26,31 @@ def add_noise(data, percent=0.7):
         data.x += noise
     return data
 
-def make_data(dataset_dict):
+def make_data(config):
     dataset = Planetoid(root='data/Planetoid', 
-                        name=dataset_dict.dataset, 
+                        name=config.dataset_name, 
                         transform=NormalizeFeatures())
     data = dataset[0]
-    data = add_noise(data, percent=dataset_dict.noise_percent)
+    data = add_noise(data, percent=config.noise_percent)
     num_features = dataset.num_features
     num_classes = dataset.num_classes
     return data, num_features, num_classes
 
-def make_iterative_model(model_dict, input_dim, output_dim):
-    model = iterativeGCN(input_dim=input_dim,
+def make_models(config, input_dim, output_dim):
+    iterative_model = iterativeGCN(input_dim=input_dim,
                             output_dim=output_dim,
-                            hidden_dim=model_dict.hid_dim,
-                            num_train_iter=model_dict.num_iterations,
-                            smooth_fac=model_dict.smooth_fac,
-                            dropout=model_dict.dropout)
-    return model
-
-def make_normal_model(model_dict, input_dim, output_dim):
-    model = GCN(input_dim=input_dim,
+                            hidden_dim=config.hid_dim,
+                            num_train_iter=config.num_iterations,
+                            smooth_fac=config.smooth_fac,
+                            dropout=config.dropout)
+    normal_model = GCN(input_dim=input_dim,
                 output_dim=output_dim,
-                hidden_dim=model_dict.hid_dim,
-                num_layers=model_dict.num_layers,
-                dropout=model_dict.dropout)
-    return model
+                hidden_dim=config.hid_dim,
+                num_layers=config.num_layers,
+                dropout=config.dropout)
+    return iterative_model, normal_model
+
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -76,77 +75,80 @@ def validate_epoch(model, data):
     acc = accuracy(pred, data.y[data.val_mask])
     return loss, acc
 
+def train(model, data, config, num_epochs, iter=True):
+    
+    optimizer = optim.Adam(model.parameters(), lr=config.learning_rate, weight_decay=config.weight_decay)
+    for epoch in range(num_epochs):
+        loss_train, acc_train = train_epoch(model, data, optimizer)
+        loss_val, acc_val = validate_epoch(model, data)
+
+    if iter:
+        wandb.log({
+            'i_training_loss': loss_train,
+            'i_training_accuracy': acc_train,
+            'i_validation_loss': loss_val,
+            'i_validation_accuracy': acc_val,
+            "epoch": epoch+1,
+        })
+    else:
+        wandb.log({
+            'n_training_loss': loss_train,
+            'n_training_accuracy': acc_train,
+            'n_validation_loss': loss_val,
+            'n_validation_accuracy': acc_val,
+            "epoch": epoch+1,
+        })
+  
+
 def test(model, data):
     model.eval()
     output = model(data.x, data.edge_index)
     loss = F.nll_loss(output[data.test_mask], data.y[data.test_mask])
     pred = output[data.test_mask].argmax(dim=1)
     acc = accuracy(pred, data.y[data.test_mask])
-    wandb.log({
-        'test_loss': loss,
-        'test_accuracy': acc
-    })
+    
     
     return loss, acc
 
-def train(model, data,optimizer_dict, num_epochs):
-    optimizer = optim.Adam(model.parameters(), lr=optimizer_dict.learning_rate, weight_decay=optimizer_dict.weight_decay)
-    for epoch in range(num_epochs):
-        loss_train, acc_train = train_epoch(model, data, optimizer)
-        loss_val, acc_val = validate_epoch(model, data)
-        wandb.log({
-            'training_loss': loss_train,
-            'training_accuracy': acc_train,
-            'validation_loss': loss_val,
-            'validation_accuracy': acc_val,
-            "epoch": epoch+1
-        })
-
-
 def run_exp(config=None):
-     with wandb.init(config=config):
+     with wandb.init(config=config, project="IterativeMethods", job_type="exp_param"):
         config = wandb.config
 
-        data, num_features, num_classes = make_data(config.dataset)
-        iterative_model = make_iterative_model(config.iterative_model, 
+        data, num_features, num_classes = make_data(config)
+        iterative_model, normal_model = make_models(config, 
                                                input_dim=num_features, 
                                                output_dim=num_classes)
-        normal_model = make_normal_model(config.normal_model, 
-                                         input_dim=num_features,
-                                         output_dim=num_classes)
         iterative_model_param = count_parameters(iterative_model)
         normal_model_param = count_parameters(normal_model)
         wandb.log({
-            'Number of parameters for the iterative model': iterative_model_param,
-            'Number of parameters for the normal model': normal_model_param
+            'i_num_param': iterative_model_param,
+            'n_num_param': normal_model_param
         })
         
-        train(iterative_model, data, config.optimizer, config.num_epochs)
-        test(iterative_model, data)
+        train(iterative_model, data, config, config.num_epochs, iter=True)
+        loss_iter, acc_iter = test(iterative_model, data)
+        wandb.log({
+        'iter_test_loss': loss_iter,
+        'iter_test_accuracy': acc_iter
+        })
+        train(normal_model, data, config, config.num_epochs, iter=False)
+        loss_normal, acc_normal = test(normal_model, data)
+        wandb.log({
+        'normal_test_loss': loss_normal,
+        'normal_test_accuracy': acc_normal
+        })
 
 config = {
-    'num_epochs': 200
-} 
-dataset_dict = {
-    'dataset': "Cora",
-    'noise_percent': 0.7
-}
-config['dataset'] = dataset_dict
-iterative_model_dict = {
+    'num_epochs': 200,
+    'dataset_name': "Cora",
+    'noise_percent': 0.7,
     'hid_dim': 32,
-    'num_train_iter': 2,
+    'num_iterations': 10,
+    'num_layers': 10,
     'smooth_fac': 0.7,
-    'dropout': 0.5
-}
-config['iterative_model'] = iterative_model_dict
-normal_model_dict = {
-    'hid_dim': 32,
-    'num_layers': 2,
-    'dropout': 0.5
-}
-config['normal_model'] = normal_model_dict
-optimizer_dict = {
+    'dropout': 0.5,
     'learning_rate': 0.01,
     'weight_decay': 5e-4
-}
-config['optimizer'] = optimizer_dict
+} 
+
+run_exp(config)
--- GCNexperiments/models.py
+++ GCNexperiments/models.py
@@ -26,22 +26,50 @@ class GCN(nn.Module):
                  dropout=0.5):
         super().__init__()
         self.first_gc = GCNConv(input_dim, hidden_dim)
-        # self.gcs = None
-        # if num_layers >=3:
-        #     self.gcs = nn.ModuleList([GCNConv(hidden_dim, hidden_dim) for i in range(num_layers-2)])
+        self.gcs = None
+        if num_layers >=3:
+            self.gcs = nn.ModuleList([GCNConv(hidden_dim, hidden_dim) for i in range(num_layers-2)])
         self.final_gc = GCNConv(hidden_dim, output_dim)
         self.dropout = dropout
 
     def forward(self, x, edge_index):
         x = F.relu(self.first_gc(x, edge_index))
         x = F.dropout(x, self.dropout, training=self.training)
-        # if self.gcs is not None:
-        #     for layer in self.gcs:
-        #         x = F.relu(layer(x, edge_index))
-        #         x = F.dropout(x, self.dropout, training=self.training)
+        if self.gcs is not None:
+            for layer in self.gcs:
+                x = F.relu(layer(x, edge_index))
+                x = F.dropout(x, self.dropout, training=self.training)
         x = self.final_gc(x, edge_index)
         return F.log_softmax(x, dim=1)
     
+
+class exp_GCN(nn.Module):
+    '''
+    A GCN model with number of layers as input. But in general, never do more than 5 layers.
+    '''
+    def __init__(self, 
+                 input_dim, 
+                 output_dim,
+                 hidden_dim,
+                 num_layers=2,
+                 dropout=0.5):
+        super().__init__()
+        self.first_lin = nn.Linear(input_dim, hidden_dim)
+        self.gcs = None
+        if num_layers >=3:
+            self.gcs = nn.ModuleList([GCNConv(hidden_dim, hidden_dim) for i in range(num_layers-2)])
+        self.final_lin = nn.Linear(hidden_dim, output_dim)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.first_lin(x, edge_index))
+        x = F.dropout(x, self.dropout, training=self.training)
+        if self.gcs is not None:
+            for layer in self.gcs:
+                x = F.relu(layer(x, edge_index))
+                x = F.dropout(x, self.dropout, training=self.training)
+        x = self.final_lin(x, edge_index)
+        return F.log_softmax(x, dim=1)
 
 class iterativeGCN(nn.Module):
     def __init__(self, 
